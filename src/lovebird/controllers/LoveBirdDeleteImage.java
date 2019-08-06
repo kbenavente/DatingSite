e14fe432b80356f16d0a88cb2cb0ab6676337efd
kbenavente <benavente.kevin@gmail.com>
2019-08-06 01:48:19 -0700

    Create LoveBirdDeleteImage.java

--- src/lovebird/controllers/LoveBirdDeleteImage.java
+++ src/lovebird/controllers/LoveBirdDeleteImage.java
@@ -1,11 +1,16 @@
 package lovebird.controllers;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import lovebird.utils.Utilities;
 
 @WebServlet("/LoveBirdDeleteImage")
 public class LoveBirdDeleteImage extends HttpServlet {
@@ -19,6 +24,50 @@ public class LoveBirdDeleteImage extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		HttpSession session = request.getSession();
+		
+		if(session.getAttribute("username") == null) {
+			
+			response.sendRedirect("LoveBirdSignUp");
+			
+		} else {
+			
+			// Get the image ID
+			
+			int image_id = Integer.parseInt(request.getParameter("imageID"));
+			
+			// Get userID
+			
+			String userID = Utilities.getUserID((String) session.getAttribute("username"));
+			int user_id = Integer.parseInt(userID);
+			
+			// Get file name 
+			
+			String fileName = Utilities.getImageName(image_id, user_id);
+			
+			// Path to file 
+			
+			String file_path = getServletContext().getRealPath("WEB-INF/files/" + fileName);
+			
+			// Delete the file
+			
+			File file = new File(file_path);
+			
+			if(file.delete()) { // Successful file delete
+				
+				Utilities.upateImageRecord(userID, image_id, null);
+				
+				response.sendRedirect("LoveBirdProfile");
+				
+			} else { // Unsuccessful file delete
+				
+				response.sendRedirect("LoveBirdProfile");
+				
+			}
+			
+			
+			
+			
+		}
 		
 	}
 
