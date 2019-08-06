78367d5d59a4821f8da340041f9dd958dcf25f74
kbenavente <benavente.kevin@gmail.com>
2019-08-06 02:08:10 -0700

    Create LoveBirdEditProfile.java

---
+++ src/lovebird/controllers/LoveBirdEditProfile.java
@@ -0,0 +1,31 @@
+package lovebird.controllers;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/LoveBirdEditProfile")
+public class LoveBirdEditProfile extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public LoveBirdEditProfile() {
+        super();
+        
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		
+	}
+
+}
