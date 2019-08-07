ee194b3dafe85dc2454e5f880192b2aaf15174c5
kbenavente <benavente.kevin@gmail.com>
2019-08-07 01:10:05 -0700

    Handle profile edit

--- src/lovebird/controllers/LoveBirdEditProfile.java
+++ src/lovebird/controllers/LoveBirdEditProfile.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import lovebird.utils.Utilities;
+
 @WebServlet("/LoveBirdEditProfile")
 public class LoveBirdEditProfile extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -35,7 +37,28 @@ public class LoveBirdEditProfile extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		String bio = request.getParameter("bio");
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+		String preference = request.getParameter("preference");
 		
+		String fieldsCannotBeEmptyError = "";
+		
+		if(firstName.isEmpty() || lastName.isEmpty() || preference.isEmpty()) {
+			
+			fieldsCannotBeEmptyError = "Note: First Name, Last Name, and Gender Preference cannot be negative";
+			getServletContext().setAttribute("fieldsCannotBeEmptyError", fieldsCannotBeEmptyError);
+			response.sendRedirect("LoveBirdEditProfile");
+			
+		} else {
+			
+			HttpSession session = request.getSession();
+			
+			Utilities.updateUserProfile((String)session.getAttribute("username"), bio, firstName, lastName, preference);
+			Utilities.saveUserInfoToSession(session, (String)session.getAttribute("username"));
+			response.sendRedirect("LoveBirdProfile");
+			
+		}
 		
 	}
 
